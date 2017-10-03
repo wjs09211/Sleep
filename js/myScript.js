447a9f194fe771932c6b24db4a0f2e9607fa9c76
sleep <wjs09211@gmail.com>
2017-10-04 00:07:23 +0800

    change name

--- js/myScript.js
+++ js/myScript.js
@@ -1,5 +1,14 @@
 $(function(){
 
-   console.log("test");
+ 	var pre_pos = 0
+ 	$(window).scroll(function() {
+ 		if($(document).scrollTop() - pre_pos > 0){
+ 			$('.navbar').addClass('hide').removeClass('show');
+ 		}
+ 		else{
+ 			$('.navbar').addClass('show').removeClass('hide');
+ 		}
+ 		pre_pos = $(document).scrollTop();
+	});
 
 });
